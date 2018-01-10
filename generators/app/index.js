6ef01a6783bcc02014679c0244b913fd4b16b136
高原 <yuan.gao@dianrong.com>
2018-01-10 17:51:55 +0800

    ocean-framework

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,6 +2,7 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const basename = require('path').basename;
 
 module.exports = class extends Generator {
   prompting() {
@@ -19,7 +20,12 @@ module.exports = class extends Generator {
         type: 'list',
         name: 'technologyStack',
         message: 'Please choose technology stack:',
-        choices: ['react-redux-rxjs', 'react-redux-saga']
+        choices: [
+          'react-redux-rxjs',
+          'react-redux-saga',
+          'ocean-framework',
+          'ocean-child'
+        ]
       }
     ];
 
@@ -32,11 +38,34 @@ module.exports = class extends Generator {
   writing() {
     const { technologyStack, language } = this.props;
 
-    this.fs.copy(
-      this.templatePath(`${technologyStack}/${language}/**`),
-      this.destinationPath('./'),
-      { globOptions: { dot: true } }
-    );
+    if (['react-redux-rxjs', 'react-redux-saga'].includes(technologyStack)) {
+      this.fs.copy(
+        this.templatePath(`${technologyStack}/${language}/**`),
+        this.destinationPath('./'),
+        { globOptions: { dot: true } }
+      );
+    } else if (technologyStack === 'ocean-framework') {
+      // 先把文件全拷贝过去
+      this.fs.copy(
+        this.templatePath(`${technologyStack}/${language}/**`),
+        this.destinationPath('./'),
+        { globOptions: { dot: true } }
+      );
+      // 重新生成需要做模版替换的文件
+      const dest = process.cwd();
+      const projectName = basename(dest);
+      const files = [
+        './package.json',
+        './readme.json',
+        './webpack.config.json',
+        './webpack.prod.config.json'
+      ];
+      const ctx = this;
+      files.forEach(_file => {
+        ctx.fs.unlinkSync(ctx.destinationPath(_file));
+        ctx.fs.copy(ctx.templatePath(_file), ctx.destinationPath(_file), { projectName });
+      });
+    }
   }
 
   install() {
